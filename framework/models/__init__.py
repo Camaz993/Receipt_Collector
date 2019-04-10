d0625596752656b4e625e963d1a2340c5d8fca0c
Alex McKirdy <ajmck@users.noreply.github.com>
2019-04-10 14:24:11 +1200

    Added remainder of REST functionality for Receipt

--- framework/models/__init__.py
+++ framework/models/__init__.py
@@ -1,2 +1,3 @@
 # import all models in this directory when importing framework.models
 from framework.models.Category import Category
+from framework.models.Receipt import Receipt
--- framework/views.py
+++ framework/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 
-from framework.serializers import CategorySerializer
-from framework.models import Category
+from framework.serializers import CategorySerializer, ReceiptSerializer
+from framework.models import Category, Receipt
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class ReceiptViewSet(viewsets.ModelViewSet):
+    queryset = Receipt.objects.all()
+    serializer_class = ReceiptSerializer
