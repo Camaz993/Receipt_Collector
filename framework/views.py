d2354ce8fb2a0bdbd5fe4c71acf3a65aeb4d3ab6
kurt <wesku620@student.otago.ac.nz>
2019-04-15 15:24:15 +1200

    Created basic search functionality for Receipts
    Included in this commit:
    - Search for a particular store (uses likeness, not exactness)
    - Filter by purchase date before, after and at
    - Filter by date update before, after and at

--- framework/views.py
+++ framework/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-
 from framework.serializers import CategorySerializer, ReceiptSerializer
 from framework.models import Category, Receipt
 
@@ -15,3 +14,39 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ReceiptViewSet(viewsets.ModelViewSet):
     queryset = Receipt.objects.all()
     serializer_class = ReceiptSerializer
+
+    def get_queryset(self):
+        queryset = Receipt.objects.all()
+        storeparam = self.request.query_params.get('store', None)
+        before_purchase = self.request.query_params.get('before_date', None)
+        after_purchase = self.request.query_params.get('after_date', None)
+        at_purchase = self.request.query_params.get('at_date', None)
+        before_update = self.request.query_params.get('before_update', None)
+        after_update = self.request.query_params.get('after_update', None)
+        at_update = self.request.query_params.get('at_update', None)
+
+        if storeparam is not None:
+            queryset = queryset.filter(store__iexact=storeparam)
+        if before_purchase is not None:
+            queryset = queryset.filter(purchase_date__lte=before_purchase)
+        if after_purchase is not None:
+                queryset = queryset.filter(purchase_date__gte=after_purchase)
+        if at_purchase is not None:
+            queryset = queryset.filter(purchase_date__exact=at_purchase)
+        if before_update is not None:
+                queryset = queryset.filter(date_updated__gte=before_update)
+        if after_purchase is not None:
+                queryset = queryset.filter(date_updated__lte=after_update)
+        if at_update is not None:
+            queryset = queryset.filter(date_updated__exact=at_update)
+
+        return queryset
+
+
+
+
+#class ReceiptFilter(filters.FilterSet):
+#    purchase_date_gte = django_filters.DateTimeFilter(name="purchase_date", lookup_expr='gte')
+    #class Meta:
+    #    model = Receipt
+    #    fields = ['store', 'total_price', 'purchase_date', 'purchase_date_gte']
